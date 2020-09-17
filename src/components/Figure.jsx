653aa03e1cac487b11921b70dcbd8ac17f272f62
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-17 09:37:26 +0200

    Implemented dynamic hangman figures

--- src/components/Figure.jsx
+++ src/components/Figure.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import stepO from '../assets/step0.jpg';
+import step0 from '../assets/step0.jpg';
 import step1 from '../assets/step1.jpg';
 import step2 from '../assets/step2.jpg';
 import step3 from '../assets/step3.jpg';
@@ -13,10 +13,13 @@ import step9 from '../assets/step9.jpg';
 import step10 from '../assets/step10.jpg';
 
 
-const Figure = () => {
+const Figure = ({ errorCount }) => {
+
+  const imageForErrorCount = [step0, step1, step2, step3, step4, step5, step6, step7, step8, step9, step10];
+  let correctFigure = imageForErrorCount.find((_, index) => index === errorCount)
   return (
     <div className="figure">
-      <img src={stepO} alt="hangman images" />
+      <img src={correctFigure} alt="hangman" />
     </div>
   );
 }
