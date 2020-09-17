b8eba110880ba3b5b89c209ae28e3e9cf28482ba
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-17 14:52:48 +0200

    implemented WrongGuesses count

--- src/components/WrongGuesses.jsx
+++ src/components/WrongGuesses.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 
-const WrongGuesses = () => (
+const WrongGuesses = ({ errorCount, maxErrors }) => (
 
   <div className="wrong-guesses">
     <h4>Wrong guesses</h4>
-    <h3>4 / 5 </h3>
+    <h3>{errorCount} / {maxErrors} </h3>
   </div>
 )
+
+WrongGuesses.propTypes = {
+  errorCount: PropTypes.number.isRequired
+}
 
 export default WrongGuesses;
