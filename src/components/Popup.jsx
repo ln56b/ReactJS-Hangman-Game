21ffe1cd3060ed00827e62ee0f7c2c0f5727c423
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-17 15:53:51 +0200

    Implemented popUp + propTypes

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 
 const Popup = ({ isWinner, isFinished }) => (
   <div className={!isFinished ? "popup" : "show"} >
     <p>
-      {isWinner ? "You won !" : "Sorry, try again"}
+      {isWinner ? "You won !" : "Sorry, you lost"}
     </p>
   </div>
 )
+
+Popup.propTypes = {
+  isWinner: PropTypes.bool.isRequired,
+  isFinished: PropTypes.bool.isRequired
+}
 
 export default Popup;
