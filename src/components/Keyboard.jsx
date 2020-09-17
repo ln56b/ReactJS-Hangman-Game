1e9cda7f699fd0b9830203d4f8472b8ddec604a0
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-17 14:37:21 +0200

    Implemented handleGuess method

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const alphabet = "ABCDEFGHIJKLMNOPKRSTUVWXYZ".toLowerCase().split('');
+const alphabet = "ABCDEFGHIJKLMNOPKRSTUVWXYZ".split('');
 
-const Keyboard = () => (
-  <div className="keyboard">
-    {alphabet.map(letter =>
-      <button>{letter}</button>
-    )}
 
-  </div>
-)
+const Keyboard = ({ handleGuess }) => {
+
+  function handleClick(e) {
+    handleGuess(e.target.value)
+  }
+
+  return (
+    <div className="keyboard">
+      {alphabet.map((letter, index) =>
+        <button key={index} onClick={handleClick} value={letter}>{letter}</button>
+      )}
+
+    </div>
+  )
+}
+
+Keyboard.propTypes = {
+  handleGuess: PropTypes.func.isRequired
+}
 
 export default Keyboard;
