7ed0821c44edc6024f12f86d823a87181d934a9e
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-18 15:06:57 +0200

    Updates

---
+++ precache-manifest.293690a7b186c0df66bf099e37d769d6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc19a83c02e76566a7dec7c611aacb22",
+    "url": "/ReactJS-Hangman-Game/index.html"
+  },
+  {
+    "revision": "fc4776c9de6fcd6f243d",
+    "url": "/ReactJS-Hangman-Game/static/css/main.715821f9.chunk.css"
+  },
+  {
+    "revision": "04d5f0504274ae854e0d",
+    "url": "/ReactJS-Hangman-Game/static/js/2.7959eadd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS-Hangman-Game/static/js/2.7959eadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4776c9de6fcd6f243d",
+    "url": "/ReactJS-Hangman-Game/static/js/main.33d09cab.chunk.js"
+  },
+  {
+    "revision": "ef00d48f19cca0d63e99",
+    "url": "/ReactJS-Hangman-Game/static/js/runtime-main.667d4190.js"
+  },
+  {
+    "revision": "32155cd33b313ac6a3bc08ea83c36e39",
+    "url": "/ReactJS-Hangman-Game/static/media/crows.32155cd3.jpg"
+  },
+  {
+    "revision": "aff1a0353faf43f9deec4b9d36304364",
+    "url": "/ReactJS-Hangman-Game/static/media/fireworks.aff1a035.jpg"
+  },
+  {
+    "revision": "f06df755b65f5f4d5c3b0a27a2d38f57",
+    "url": "/ReactJS-Hangman-Game/static/media/step0.f06df755.jpg"
+  },
+  {
+    "revision": "ca6c10d5826783b2e21485e1e92df1ce",
+    "url": "/ReactJS-Hangman-Game/static/media/step1.ca6c10d5.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/ReactJS-Hangman-Game/static/media/step10.c80394c2.jpg"
+  },
+  {
+    "revision": "1dc765683556a2c05162544cff514491",
+    "url": "/ReactJS-Hangman-Game/static/media/step2.1dc76568.jpg"
+  },
+  {
+    "revision": "837148ce5a3b2f485463adac61b033ec",
+    "url": "/ReactJS-Hangman-Game/static/media/step3.837148ce.jpg"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/ReactJS-Hangman-Game/static/media/step4.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/ReactJS-Hangman-Game/static/media/step5.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/ReactJS-Hangman-Game/static/media/step6.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/ReactJS-Hangman-Game/static/media/step7.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/ReactJS-Hangman-Game/static/media/step8.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/ReactJS-Hangman-Game/static/media/step9.1b557aa7.jpg"
+  }
+]);
