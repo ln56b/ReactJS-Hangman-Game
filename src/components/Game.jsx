d5e3f7b0717c4548a24d3f291ec7b1f42026a9db
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-18 09:52:32 +0200

    Functional Win & Lose state

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,27 +1,80 @@
-import React, { useState } from 'react'
-import Figure from './Figure'
-import Header from './Header'
-import Keyboard from './Keyboard'
-import Popup from './Popup'
-import Word from './Word'
-import WrongGuesses from './WrongGuesses'
+import React, { useState, useEffect } from 'react';
+import Figure from './Figure';
+import Header from './Header';
+import Keyboard from './Keyboard';
+import Popup from './Popup';
+import ResetGame from './ResetGame';
+import Word from './Word';
+import WrongGuesses from './WrongGuesses';
+
+const MAX_ERRORS = 10;
+const WORDS = ['almond', 'apricot', 'cashew', 'date', 'fig', 'fruit', 'grape', 'hazelnut', 'leaf', 'lemon', 'melon', 'olive', 'peach'];
+
 
 const Game = () => {
 
-  const [hiddenWord, setHiddenWord] = useState('Hello');
+  const [hiddenWord, setHiddenWord] = useState('');
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [errorCount, setErrorCount] = useState(0);
+  const [isWinner, setIsWinner] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+  const [isPlayable, setIsPlayable] = useState(true);
 
-  const isWinner = false;
-  const isFinished = false;
+
+  useEffect(() => {
+    checkGameScore()
+  })
+
+  function checkGameScore() {
+    if (hiddenWord !== '') {
+      if (hiddenWord.length === guessedLetters.length) {
+        setIsFinished(true)
+        setIsPlayable(false)
+        setIsWinner(true)
+      }
+      else if (errorCount === MAX_ERRORS) {
+        setIsFinished(true)
+        setIsPlayable(false)
+      }
+    }
+  }
+
+  function generateRandomWord() {
+    let newWord = WORDS[Math.floor(Math.random() * WORDS.length)].toUpperCase();
+    setHiddenWord(newWord);
+    return newWord;
+  }
+
+
+  function handleGuess(letter) {
+    if (hiddenWord.includes(letter)) {
+      setGuessedLetters(() => [...guessedLetters, letter])
+    } else {
+      setWrongLetters(() => [...wrongLetters, letter])
+      setErrorCount(errorCount + 1)
+    }
+  }
+
+  function resetGame() {
+    setHiddenWord('')
+    setGuessedLetters([])
+    setErrorCount(0)
+    setIsWinner(false)
+    setIsFinished(false)
+    setIsPlayable(true)
+  }
 
   return (
-    <>
+    <React.Fragment>
       <Header />
-      <Figure />
-      <WrongGuesses />
-      <Word hiddenWord={hiddenWord} />
-      <Keyboard />
+      <Figure errorCount={errorCount} isWinner={isWinner} />
+      <Keyboard handleGuess={handleGuess} hiddenWord={hiddenWord} guessedLetters={guessedLetters} wrongLetters={wrongLetters} />
+      <WrongGuesses errorCount={errorCount} />
+      <Word generateRandomWord={generateRandomWord} hiddenWord={hiddenWord} guessedLetters={guessedLetters} />
       <Popup isWinner={isWinner} isFinished={isFinished} />
-    </>
+      <ResetGame isPlayable={isPlayable} resetGame={resetGame} />
+    </React.Fragment >
   );
 }
 
